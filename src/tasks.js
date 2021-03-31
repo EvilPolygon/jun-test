a3996334ea7eb067b284c1b12bc970e0ef956170
EvilPolygon <serega.net7@gmail.com>
2021-03-31 21:26:24 +0500

    back to work

---
+++ src/tasks.js
@@ -0,0 +1,19 @@
+'use strict';
+alert('asdassad');
+class Test extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return e(
+            <h1>asdasd</h1>
+        );
+    }
+}
+
+//------------------------------------------
+ReactDOM.render(
+    <Test />,
+    document.getElementById('root')
+  );
