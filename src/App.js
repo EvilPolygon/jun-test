0d1a7a044a53f22fdb970474c2721d9bbd287ca9
EvilPolygon <serega.net7@gmail.com>
2021-04-16 14:15:08 +0500

    move

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
-//import React, { Component} from "react";
+import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
 
-class App extends Component{
+class Abitbut extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={value:true};
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(){
+    if(this.state.value){
+      alert('nice');
+    }
+    else{
+      alert('very nice');
+    }
+
+    this.setState(prevState => ({value: !prevState.value}));
+  }
+
   render(){
     return(
+      <button onClick = {this.onClick}>
+        wow
+      </button>
+    )
+  }
+}
+
+class Test extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1> some great changes <Abitbut /></h1>
+      </div>
+    )
+  }
+}
+
+class Pages extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isLoggedIn: false};    
+  }
+
+  render(){
+    let page;
+    if(this.state.isLoggedIn){
+      page = <h1>login true</h1>;
+    }
+    else{
+      return(
+        <div className = "welcome">
+          <h1 >Добро пожаловать!</h1>
+          <h2 >Это тестовое задание для обучения веб-программированию и проверке знаний</h2>
+          <h5 >Для продолжения необходимо войти или зарегистрироваться</h5>
+          <Abitbut /> <br></br>
+          <Abitbut />
+        </div>
+      )
+    }
+  }
+}
+
+class App extends Component{
+  render(){ 
+    return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <Pages />
       </div>
     );
   }
